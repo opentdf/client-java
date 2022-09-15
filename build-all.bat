beda6a7deadbf36ba7eaa5382c930a35ce46359f
Pat Mancuso <pmancuso@virtru.com>
2022-09-15 12:24:39 -0400

    Fix env var, fix zipfile creation

--- build-all.bat
+++ build-all.bat
@@ -17,15 +17,7 @@ conan build .. --build-folder .
 set builderrorlevel=%errorlevel%
 if %builderrorlevel% neq 0 goto fin
 
-REM REM for some reason the cmake install isn't working, so copy these to the install area now
-REM mkdir %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\include\
-REM copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\csharp-bindings\source\*.cs %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\include\
-REM 
-REM mkdir %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\lib\
-REM copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\lib\*.lib %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\lib\
-REM copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\lib\*.dll %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\lib\
-REM 
-REM powershell -command Compress-Archive -Force -Path %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\ -CompressionLevel Optimal -DestinationPath %PROJECT_DIR%\%TDF_JAVA_OUTPUT%%TDF_ZIP_SUFFIX%.zip
+powershell -command Compress-Archive -Force -Path %PROJECT_DIR%\%TDF_JAVA_OUTPUT%\ -CompressionLevel Optimal -DestinationPath %PROJECT_DIR%\%TDF_JAVA_OUTPUT%%TDF_ZIP_SUFFIX%.zip
 
 :fin
 REM return to where we came from
