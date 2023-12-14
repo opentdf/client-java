b5c55b263b32c39d7958e03fd819f7d89396cdd4
Pat Mancuso <pmancuso@virtru.com>
2023-12-14 15:23:44 -0500

    Update curl past obsolete version

--- docker/linux-arm/install_req.sh
+++ docker/linux-arm/install_req.sh
@@ -11,7 +11,7 @@ apt-get update &&  apt-get install -y --no-install-recommends build-essential=12
   openjdk-11-jdk=11.0.21+9-0ubuntu1~22.04 \
   maven=3.6.3-5 \
   swig=4.0.2-1ubuntu1 \
-  curl=7.81.0-1ubuntu1.14 \
+  curl=7.81.0-1ubuntu1.15 \
   jq=1.6-2.1ubuntu3 \
   python3-pip=22.0.2+dfsg-1ubuntu0.4 \
   && rm -rf /var/lib/apt/lists/*
